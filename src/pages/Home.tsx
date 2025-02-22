62c21dc8de8432f9ebdc6e38140c4e1c601494d8
Thomas-NS <thomasnecionis@gmai.com>
2025-02-22 21:17:01 +0000

    Added basic structure of homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,60 @@
+import styles from './Home.module.css';
+import { Link } from 'react-router-dom';
+
 const Home = () => {
+	console.log(Math.random());
+
 	return (
-		<h1>  </h1>
+		<>
+			<section className={styles.homeContainer}>
+				<div className={styles.homeLeftColumn}>
+					<article className={styles.callToAction}>
+						<h2 className={styles.callToActionText}>Help protect Ireland’s wildlife - every small action can make a big impact.</h2>
+						<button className={styles.callToActionBtn}> <Link to={"/conservation"}>TAKE ACTION</Link></button>
+					</article>
+
+					<article className={styles.animalGameContainer}>
+						<div className={styles.sectionHeadingContainer}> 
+							<div className={styles.sectionHeadingLine}></div>
+							<h2 className={styles.sectionHeading}>Can you name this critter?</h2>
+							<div className={styles.sectionHeadingLine}></div>
+						</div>
+						<div className={styles.animalGameMainContainer}>
+							<div className={styles.gameWordContainer}>
+								<pre className={styles.gameWordText}>________ ________</pre>
+							</div>
+
+							<div className={styles.gameBtnContainer}>
+								<button className={styles.hintBtn}>? Hint 0/5</button>
+								<button className={styles.giveupBtn}>Give Up</button>
+							</div>
+							<img className={styles.animalGameImg} src="/placeholder.png" alt="American Woodcock facing towards the camera"/>
+						</div>
+					</article>
+				</div>
+
+				<div className={styles.homeRightColumn}>
+					<article className={styles.creatureFeatureContainer}>
+						<div className={styles.sectionHeadingContainer}> 
+							<div className={styles.sectionHeadingLine}></div>
+							<h2 className={styles.sectionHeading}>Creature Feature</h2>
+							<div className={styles.sectionHeadingLine}></div>
+						</div>
+						<img className={styles.creatureFeatureImg} src="/placeholder.png" alt="American Woodcock facing towards the camera"/>
+					</article>
+
+					<article className={styles.wildFactContainer}>
+						<div className={styles.sectionHeadingContainer}> 
+							<div className={styles.sectionHeadingLine}></div>
+								<h2 className={styles.sectionHeading}>Wild Fact</h2>
+							<div className={styles.sectionHeadingLine}></div>
+						</div>
+						<p className={styles.wildFactText}>Ireland is home to over 11,000 species of insects!</p>
+					</article>
+				</div>
+
+			</section>
+		</>
 	);
 };
 
