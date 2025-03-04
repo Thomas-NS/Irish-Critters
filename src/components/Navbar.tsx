a493926b1247a2b481430ea3a5d6fafa6835b2ec
Thomas-NS <thomasnecionis@gmai.com>
2025-03-04 11:26:01 +0000

    Removed unnecessary whitespace

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,27 +6,27 @@ const Navbar = () => {
 
 	return (
 		<>
-		<header className={styles.header}>
+			<header className={styles.header}>
 				<img className={styles.logo} src="/lapwing.png" alt="Silhouette of a northern lapwing"/>
 				<h1 className={styles.siteTitle}>IRISH CRITTERS</h1>
-		</header>
+			</header>
 
-		<nav className={styles.navbar}>
-			<ul className={styles.navList}>
-				<li> <Link to="/critters" className={location.pathname === "/critters" ? 
-					styles.active : styles.inactive }>CRITTERS</Link> </li>
+			<nav className={styles.navbar}>
+				<ul className={styles.navList}>
+					<li> <Link to="/critters" className={location.pathname === "/critters" ? 
+						styles.active : styles.inactive }>CRITTERS</Link> </li>
 
-				<li> <Link to="/" className={location.pathname === "/" ? 
-					styles.active : styles.inactive }>HOME</Link> </li>
+					<li> <Link to="/" className={location.pathname === "/" ? 
+						styles.active : styles.inactive }>HOME</Link> </li>
 
-				<li> <Link to="/conservation" className={location.pathname === "/conservation" ? 
-					styles.active : styles.inactive }>CONSERVATION</Link> </li>
+					<li> <Link to="/conservation" className={location.pathname === "/conservation" ? 
+						styles.active : styles.inactive }>CONSERVATION</Link> </li>
 
-				<li> <Link to="/quiz" className={location.pathname === "/quiz" ? 
-					styles.active : styles.inactive }>QUIZ</Link> </li>
-			</ul>
-			<input type="text" className={styles.searchbar} placeholder="Search"/>
-		</nav>
+					<li> <Link to="/quiz" className={location.pathname === "/quiz" ? 
+						styles.active : styles.inactive }>QUIZ</Link> </li>
+				</ul>
+				<input type="text" className={styles.searchbar} placeholder="Search"/>
+			</nav>
 		</>
 	);
 };
